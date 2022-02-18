1b463576ce108e60bcc63cbd6c2396cb496f92b5
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-18 20:51:26 +0100

    Added function call to connect to the database in the IIFE

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const debug = require("debug")("robots:root");
 const chalk = require("chalk");
+const databaseConnection = require("./database");
 const deployServer = require("./server");
 
 const port = process.env.SERVER_PORT || 6254;
+const robotsDatabase = process.env.ROBOTS_DB;
 
 (async () => {
   try {
+    await databaseConnection(robotsDatabase);
     await deployServer(port);
   } catch (error) {
     debug(chalk.bgRed.white(error.message));
