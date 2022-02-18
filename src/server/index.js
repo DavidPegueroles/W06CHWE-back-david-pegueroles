563fd7f30e1170d4b6b519e53496efaad31af992
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-18 22:00:22 +0100

    Added errors middlewares in server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,8 @@
+const debug = require("debug")("robots:server");
 const chalk = require("chalk");
 const express = require("express");
 const morgan = require("morgan");
-const debug = require("debug")("robots:server");
+const { notFoundError, generalError } = require("./middlewares/errors");
 
 const app = express();
 
@@ -23,5 +24,8 @@ app.use((req, res, next) => {
   debug(chalk.bgBlack.magenta(`A request has arrived to ${req.url}`));
   next();
 });
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = deployServer;
