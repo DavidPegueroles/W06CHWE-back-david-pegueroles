f362ec830ded0b5693ba52f4fbdfa6e8a6b38b69
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-18 22:06:24 +0100

    Added return in databaseConnection function to stop in case of error

--- src/database/index.js
+++ src/database/index.js
@@ -7,6 +7,7 @@ const databaseConnection = (url) =>
     mongoose.connect(url, (error) => {
       if (error) {
         reject(new Error("Couldn't connect to the database"));
+        return;
       }
       debug(chalk.green("Connected to the database"));
       resolve();
