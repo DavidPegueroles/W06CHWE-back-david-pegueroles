a1693acd55ac251611e394f866c3438cb5fa27db
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-20 21:25:31 +0100

    Added test to deleteRobot

--- src/server/controllers/robotsControllers.test.js
+++ src/server/controllers/robotsControllers.test.js
@@ -1,5 +1,5 @@
 const Robot = require("../../database/models/Robot");
-const { getRobots, getARobot } = require("./robotsControllers");
+const { getRobots, getARobot, deleteRobot } = require("./robotsControllers");
 
 jest.mock("../../database/models/Robot");
 
@@ -11,6 +11,7 @@ describe("Given a getRobots controller", () => {
   describe("When it receives a response", () => {
     test("Then it should call method json of the received response", async () => {
       const res = {
+        status: jest.fn(),
         json: jest.fn(),
       };
       const robots = [
@@ -45,6 +46,7 @@ describe("Given a geARobot controller", () => {
         },
       };
       const res = {
+        status: jest.fn(),
         json: jest.fn(),
       };
       const robot = [
@@ -65,6 +67,41 @@ describe("Given a geARobot controller", () => {
       await getARobot(req, res);
 
       expect(Robot.findById).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalledWith({ robot });
+    });
+  });
+});
+
+describe("Given a deleteRobot controller", () => {
+  describe("When it receives a response with an id in the params", () => {
+    test("Then it should call status and json methods of the received response", async () => {
+      const req = {
+        params: {
+          _id: "1",
+        },
+      };
+      const res = {
+        status: jest.fn(),
+        json: jest.fn(),
+      };
+      const robot = [
+        {
+          characteristics: {
+            velocity: 9,
+            resistance: 7,
+            creation_date: "",
+          },
+          _id: "1",
+          name: "Luis",
+          url: "",
+        },
+      ];
+
+      Robot.findByIdAndDelete = jest.fn().mockResolvedValue(robot);
+
+      await deleteRobot(req, res);
+
+      expect(Robot.findByIdAndDelete).toHaveBeenCalled();
       expect(res.json).toHaveBeenCalledWith({ robot });
     });
   });
