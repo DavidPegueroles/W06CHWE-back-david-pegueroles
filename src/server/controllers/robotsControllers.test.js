203f8fb388aeb57a8cd7c3105a0b61e58ce2af59
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-20 12:55:55 +0100

    Added test to getARobot controller function

--- src/server/controllers/robotsControllers.test.js
+++ src/server/controllers/robotsControllers.test.js
@@ -1,13 +1,13 @@
 const Robot = require("../../database/models/Robot");
-const { getRobots } = require("./robotsControllers");
+const { getRobots, getARobot } = require("./robotsControllers");
 
 jest.mock("../../database/models/Robot");
 
-describe("Given a getRobots controller", () => {
-  beforeEach(() => {
-    jest.resetAllMocks();
-  });
+beforeEach(() => {
+  jest.resetAllMocks();
+});
 
+describe("Given a getRobots controller", () => {
   describe("When it receives a response", () => {
     test("Then it should call method json of the received response", async () => {
       const res = {
@@ -32,6 +32,40 @@ describe("Given a getRobots controller", () => {
 
       expect(Robot.find).toHaveBeenCalled();
       expect(res.json).toHaveBeenCalledWith({ robots });
+    });
+  });
+});
+
+describe("Given a geARobot controller", () => {
+  describe("When it receives a response with an id in the params", () => {
+    test("Then it should call method json of the received response", async () => {
+      const req = {
+        params: {
+          _id: "1",
+        },
+      };
+      const res = {
+        json: jest.fn(),
+      };
+      const robot = [
+        {
+          characteristics: {
+            velocity: 9,
+            resistance: 7,
+            creation_date: "",
+          },
+          _id: "1",
+          name: "Luis",
+          url: "",
+        },
+      ];
+
+      Robot.findById = jest.fn().mockResolvedValue(robot);
+
+      await getARobot(req, res);
+
+      expect(Robot.findById).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalledWith({ robot });
     });
   });
 });
