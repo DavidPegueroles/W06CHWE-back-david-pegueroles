c513aa00a82f0702ed644c3a9c58969e2a6c9367
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-20 21:24:33 +0100

    Added route to delete requests

--- src/server/routers/robotsRouter.js
+++ src/server/routers/robotsRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getRobots, getARobot } = require("../controllers/robotsControllers");
+const {
+  getRobots,
+  getARobot,
+  deleteRobot,
+} = require("../controllers/robotsControllers");
 
 const router = express.Router();
 
 router.get("/", getRobots);
 router.get("/:idRobot", getARobot);
+router.delete("/delete/:idRobot", deleteRobot);
 
 module.exports = router;
