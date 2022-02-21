b3170a509cf9885787873a53e7bd2c134141b655
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-21 21:40:13 +0100

    Added User model

--- src/database/index.js
+++ src/database/index.js
@@ -5,6 +5,16 @@ const mongoose = require("mongoose");
 const databaseConnection = (url) =>
   new Promise((resolve, reject) => {
     mongoose.connect(url, (error) => {
+      mongoose.set("debug", true);
+      mongoose.set("toJSON", {
+        virtuals: true,
+        transform: (doc, ret) => {
+          // eslint-disable-next-line no-param-reassign, no-underscore-dangle
+          delete ret._id;
+          // eslint-disable-next-line no-param-reassign, no-underscore-dangle
+          delete ret.__v;
+        },
+      });
       if (error) {
         reject(new Error("Couldn't connect to the database"));
         return;
