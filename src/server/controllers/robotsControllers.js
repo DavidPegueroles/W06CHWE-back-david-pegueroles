067082b4bc67046c7f30e1e82f0d76e49c211387
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-22 21:08:15 +0100

    Refactorised functions to catch errors

--- src/server/controllers/robotsControllers.js
+++ src/server/controllers/robotsControllers.js
@@ -1,15 +1,31 @@
 const Robot = require("../../database/models/Robot");
 
-const getRobots = async (req, res) => {
-  const robots = await Robot.find();
-  res.status(200);
-  res.json({ robots });
+const getRobots = async (req, res, next) => {
+  try {
+    const robots = await Robot.find();
+    res.status(200).json({ robots });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const getARobot = async (req, res) => {
-  const robot = await Robot.findById(req.params.idRobot);
-  res.status(200);
-  res.json({ robot });
+const getARobot = async (req, res, next) => {
+  const id = req.params.idRobot;
+
+  try {
+    const robot = await Robot.findById(id);
+
+    if (robot) {
+      res.status(200).json({ robot });
+    } else {
+      const error = new Error("Robot does not exist.");
+      error.code = 404;
+      next(error);
+    }
+  } catch (error) {
+    error.code = 400;
+    next(error);
+  }
 };
 
 const deleteRobot = async (req, res) => {
